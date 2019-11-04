870900218ab3b9e023571b798be5d1fdd74d3b3e
sunhui <1161186762@qq,com>
2019-11-04 16:43:34 +0800

    JavaGuide

---
+++ src/JavaGuide1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class JavaGuide1 {
+    public static void main(String[] args){
+        System.out.println("开始");
+//        从键盘接收数据
+        Scanner input = new Scanner(System.in);
+        System.out.println("输入，nextLine方式接收：");
+        // nextLine 字符串接受
+        String s  = input.nextLine();
+//        input.close() 是关闭文件
+//        input.close();
+        System.out.println(s);
+    }
+}
